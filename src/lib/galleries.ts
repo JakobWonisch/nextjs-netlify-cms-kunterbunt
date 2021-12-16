5ccbbe263e9e8dd79d5c551e6307285fae1240de
JakobWonisch <jakob.wonisch@gmail.com>
2021-12-16 11:59:53 +0100

    add gallery

---
+++ src/lib/galleries.ts
@@ -0,0 +1,69 @@
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+import yaml from "js-yaml";
+
+const galleriesDirectory = path.join(process.cwd(), "content/galleries");
+
+export type GalleryContent = {
+  readonly slug: string;
+  readonly title: string;
+  readonly description: string;
+  readonly fullPath: string;
+};
+
+let galleryCache: GalleryContent[];
+
+export function fetchGalleryContent(): GalleryContent[] {
+  if (galleryCache) {
+    return galleryCache;
+  }
+  // Get file names under /pages
+  const fileNames = fs.readdirSync(galleriesDirectory);
+  const allGalleriesData = fileNames
+    .filter((it) => it.endsWith(".mdx"))
+    .map((fileName) => {
+      // Read markdown file as string
+      const fullPath = path.join(galleriesDirectory, fileName);
+      const fileContents = fs.readFileSync(fullPath, "utf8");
+
+      // Use gray-matter to parse the page metadata section
+      const matterResult = matter(fileContents, {
+        engines: {
+          yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
+        },
+      });
+      const matterData = matterResult.data as {
+        slug: string;
+        title: string;
+        description: string;
+        fullPath: string,
+      };
+      matterData.fullPath = fullPath;
+
+      const slug = fileName.replace(/\.mdx$/, "");
+
+      // Validate slug string
+      if (matterData.slug !== slug) {
+        throw new Error(
+          "slug field not match with the path of its content source"
+        );
+      }
+
+      return matterData;
+    });
+
+  return allGalleriesData;
+}
+
+export function countGalleries(tag?: string): number {
+  return fetchPageContent().length;
+}
+
+export function listGalleryContent(
+  gallery: number,
+  limit: number,
+  tag?: string
+): GalleryContent[] {
+  return fetchGalleryContent().slice((gallery - 1) * limit, gallery * limit);
+}
---
+++ src/pages/galleries/[gallery].tsx
@@ -0,0 +1,117 @@
+import { GetStaticProps, GetStaticPaths } from "next";
+import renderToString from "next-mdx-remote/render-to-string";
+import { MdxRemote } from "next-mdx-remote/types";
+import hydrate from "next-mdx-remote/hydrate";
+import matter from "gray-matter";
+import { fetchGalleryContent } from "../../lib/galleries";
+import fs from "fs";
+import yaml from "js-yaml";
+import { parseISO } from 'date-fns';
+import PageLayout from "../../components/PageLayout";
+
+import InstagramEmbed from "react-instagram-embed";
+import YouTube from "react-youtube";
+import { TwitterTweetEmbed } from "react-twitter-embed";
+
+import React from 'react';
+import { render } from 'react-dom';
+// import ResponsiveGallery from 'react-responsive-gallery';
+
+import path from "path";
+import dynamic from 'next/dynamic'
+
+export type Props = {
+  title: string;
+  slug: string;
+  description: string;
+  photos: object[];
+  footerSource: MdxRemote.Source;
+  footerSourceAddress: MdxRemote.Source;
+  source: MdxRemote.Source;
+};
+
+const components = { InstagramEmbed, YouTube, TwitterTweetEmbed };
+const slugToGalleryContent = (galleryContents => {
+  let hash = {}
+  galleryContents.forEach(it => hash[it.slug] = it)
+  return hash;
+})(fetchGalleryContent());
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('react-responsive-gallery'),
+  { ssr: false }
+)
+
+export default function GalleryPage({
+  title,
+  slug,
+  description,
+  photos,
+  footerSource,
+  footerSourceAddress,
+  source,
+}: Props) {
+  const content = hydrate(source, { components })
+  const footerContent = hydrate(footerSource, { components })
+  const footerContentAddress = hydrate(footerSourceAddress, { components })
+  return (
+    <PageLayout
+      slug={slug}
+      summary={description}
+      footer={footerContent}
+      footerAddress={footerContentAddress}
+    >
+      <h1>{title}</h1>
+      {content}
+      <DynamicComponentWithNoSSR images={photos} />
+    </PageLayout>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = fetchGalleryContent().map(it => "/galleries/" + it.slug);
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const slug = params.gallery as string;
+
+  // render page
+  const source = fs.readFileSync(slugToGalleryContent[slug].fullPath, "utf8");
+  const { content, data } = matter(source, {
+    engines: { yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object }
+  });
+  const mdxSource = await renderToString(content, { components, scope: data });
+
+  // render footer
+  const footerPathSpots = path.join(process.cwd(), "footer/spots.yml");
+  const footerSource = fs.readFileSync(footerPathSpots, "utf8");
+  const footerYaml = yaml.load(footerSource, { schema: yaml.JSON_SCHEMA }) as object;
+  const mdxFooterSource = await renderToString(footerYaml["body"], { components, scope: data });
+
+  const footerPathAddress = path.join(process.cwd(), "footer/address.yml");
+  const footerSourceAddress = fs.readFileSync(footerPathAddress, "utf8");
+  const footerYamlAddress = yaml.load(footerSourceAddress, { schema: yaml.JSON_SCHEMA }) as object;
+  const mdxFooterAddressSource = await renderToString(footerYamlAddress["body"], { components, scope: data });
+
+  // console.log(footerYaml);
+  // console.log(mdxFooterSource);
+
+  const photoset = data.photos.map(it => { return { src: it.photo }});
+
+console.log(photoset);
+  return {
+    props: {
+      title: data.title,
+      slug: data.slug,
+      description: data.description,
+      photos: photoset,
+      footerSource: mdxFooterSource,
+      footerSourceAddress: mdxFooterAddressSource,
+      source: mdxSource
+    },
+  };
+};
