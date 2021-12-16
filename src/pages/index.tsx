944ba43e7fbe92f6a13ff214cb451d4826e7b597
JakobWonisch <jakob.wonisch@gmail.com>
2021-12-16 01:01:44 +0100

    add home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,49 @@
-import Layout from "../components/Layout";
+import PageLayout from "../components/PageLayout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
 import TwitterCardMeta from "../components/meta/TwitterCardMeta";
 import { SocialList } from "../components/SocialList";
 
-export default function Index() {
+import { GetStaticProps } from "next";
+import renderToString from "next-mdx-remote/render-to-string";
+import { MdxRemote } from "next-mdx-remote/types";
+import hydrate from "next-mdx-remote/hydrate";
+import matter from "gray-matter";
+import fs from "fs";
+import yaml from "js-yaml";
+
+import InstagramEmbed from "react-instagram-embed";
+import YouTube from "react-youtube";
+import { TwitterTweetEmbed } from "react-twitter-embed";
+
+import path from "path";
+
+export type Props = {
+  footerSource: MdxRemote.Source;
+  footerSourceAddress: MdxRemote.Source;
+  source: MdxRemote.Source;
+};
+
+export default function Index({
+  footerSource,
+  footerSourceAddress,
+  source
+}: Props) {
+  const content = hydrate(source, {})
+  const footerContent = hydrate(footerSource, {})
+  const footerContentAddress = hydrate(footerSourceAddress, {})
   return (
-    <Layout>
+    <PageLayout
+      slug=""
+      summary=""
+      footer={footerContent}
+      footerAddress={footerContentAddress}>
       <BasicMeta url={"/"} />
       <OpenGraphMeta url={"/"} />
       <TwitterCardMeta url={"/"} />
-      <div className="container">
-        <div>
-          <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
-          </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
-          <SocialList />
-        </div>
-      </div>
+
+      {content}
+
       <style jsx>{`
         .container {
           display: flex;
@@ -57,6 +81,38 @@ export default function Index() {
           }
         }
       `}</style>
-    </Layout>
+    </PageLayout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  // render page
+  const sourcePath = path.join(process.cwd(), "content/pages/home.mdx");
+  const source = fs.readFileSync(sourcePath, "utf8");
+  const { content, data } = matter(source, {
+    engines: { yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object }
+  });
+  const mdxSource = await renderToString(content, { scope: data });
+
+  // render footer
+  const footerPathSpots = path.join(process.cwd(), "footer/spots.yml");
+  const footerSource = fs.readFileSync(footerPathSpots, "utf8");
+  const footerYaml = yaml.load(footerSource, { schema: yaml.JSON_SCHEMA }) as object;
+  const mdxFooterSource = await renderToString(footerYaml.body, { scope: data });
+
+  const footerPathAddress = path.join(process.cwd(), "footer/address.yml");
+  const footerSourceAddress = fs.readFileSync(footerPathAddress, "utf8");
+  const footerYamlAddress = yaml.load(footerSourceAddress, { schema: yaml.JSON_SCHEMA }) as object;
+  const mdxFooterAddressSource = await renderToString(footerYamlAddress.body, { scope: data });
+
+  // console.log(footerYaml);
+  // console.log(mdxFooterSource);
+
+  return {
+    props: {
+      footerSource: mdxFooterSource,
+      footerSourceAddress: mdxFooterAddressSource,
+      source: mdxSource
+    },
+  };
+};
